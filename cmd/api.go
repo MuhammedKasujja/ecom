610fc69b8c940890be63c2b102aadfa14c0d5501
MuhammedKasujja <al.kasmud.2@gmail.com>
2025-11-28 13:15:08 +0300

    feat: develop api interface structure that is reusable

---
+++ cmd/api.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string // user = password = dbname
+}
+
+// mount the application and return app generic [http.Handler] can be substituted with
+// chi, gin, mux
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// Middlewares
+	r.Use(middleware.RequestID) // important for rate limiting by caching the request
+	r.Use(middleware.RealIP)    // important for rate limiting and analytics and tracing
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer) // recover from crashes
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	// test service status
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Server is running..."))
+	})
+
+	return r
+}
+
+func (app *application) run(h http.Handler) error {
+	srv := &http.Server{
+		Addr: app.config.addr,
+		Handler: h,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout: time.Second * 10,
+		IdleTimeout: time.Minute,
+	}
+
+	log.Printf("server has started at addr %s", srv.Addr)
+
+	return  srv.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/MuhammedKasujja/ecom
+
+go 1.24.4
+
+require github.com/go-chi/chi/v5 v5.2.3
