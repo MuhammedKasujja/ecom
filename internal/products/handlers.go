84f7ca5b096a37c109b442771a437ca0fbbb840e
MuhammedKasujja <al.kasmud.2@gmail.com>
2025-11-28 14:56:23 +0300

    feat: format products response as JSON

--- internal/products/handlers.go
+++ internal/products/handlers.go
@@ -20,7 +20,12 @@ func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
 	// 1. Call the service -> ListProduct
 	// 2. Return JSON in the HTTP response
 
-	products := []string{"musa", "tomato"}
+	products := struct {
+		Products []string `json:"products"`
+	}{}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 
 	json.NewEncoder(w).Encode(products)
 }
